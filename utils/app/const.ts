014327d03641214a78c86b3b1a088605bac7e5e9
gauravdewan <gauravdewan@deloitte.co.uk>
2023-07-20 13:24:13 +0400

    changed system prompt

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "You are Gov Chat an AI Assistant using Azure OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  "You are Deloitte GPT, an AI Assistant using Azure OpenAI made specifically for Deloitte Practitioners. Follow the user's instructions carefully. Remember this is a corporate setting. Remind the user of their corporate responsibility whenever needed.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://openai-testing-deloitteuk.openai.azure.com/';
@@ -18,6 +18,6 @@ export const OPENAI_ORGANIZATION =
   process.env.OPENAI_ORGANIZATION || '';
 
 export const AZURE_DEPLOYMENT_ID =
-  process.env.AZURE_DEPLOYMENT_ID || '';
+  process.env.AZURE_DEPLOYMENT_ID || 'openai-test-deployment-deloitteuk';
 
 export const AZURE_APIM = process.env.AZURE_APIM_KEY
